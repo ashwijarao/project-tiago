174c89d1d013de9246153987f0693929b6130661
Sujith Rajendran <s4152352@student.rmit.edu.au>
2026-06-12 17:00:19 +1000

    Add explainer ROS publishers subscribers and action client

--- Bandit_Assist_Project/bandit_assist_ws/src/scripts/explainer_node.py
+++ Bandit_Assist_Project/bandit_assist_ws/src/scripts/explainer_node.py
@@ -69,4 +69,61 @@ def build_motion_goal(motion_name):
 
     return goal
 
+class Explainer:
+    
+    def __init__(self):
+
+        self.tts_pub = rospy.Publisher(
+
+            "/tts_web_command",
+
+            String,
+
+            queue_size=1
+
+        )
+
+        self.motion_client = actionlib.SimpleActionClient(
+
+            "/play_motion",
+
+            PlayMotionAction
+
+        )
+
+        rospy.loginfo("Waiting for play_motion action server...")
+
+        connected = self.motion_client.wait_for_server(rospy.Duration(10.0))
+
+        if connected:
+
+            rospy.loginfo("play_motion ready.")
+
+            self.motion_available = True
+
+        else:
+
+            rospy.logwarn(
+
+                "play_motion not available. Speech will work, but arm motion will be skipped."
+
+            )
+
+            self.motion_available = False
+
+        rospy.Subscriber(
+
+            "/explain",
+
+            String,
+
+            self.on_arrival
+
+        )
+
+        rospy.loginfo("Explainer node started.")
+
+    def on_arrival(self, msg):
+
+        pass
 
